271d2cbebb12bc41d1eef3beaafaea8378845c91
vardaansharma096 <vardaansharma096@gmail.com>
2023-09-26 18:00:04 +0530

    commit the path mentioned in the file

--- docker_shell_script.sh
+++ docker_shell_script.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cd /var/lib/jenkins/workspace/docker
+#cd /var/lib/jenkins/workspace/docker
 #The --build flag tells Docker Compose to rebuild the images used by your services, even if they already exist.
 sudo docker-compose up -d --build
